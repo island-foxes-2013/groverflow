aa9ed6e2b404fcb044eb41cceca9eed6517096a5
Apprentice <apprentice@dbc19.local>
2013-08-07 15:14:35 -0700

    Added question model tests

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -11,12 +11,45 @@ describe Question do
       it "should return an instance of question" do
         expect(question).to be_a Question
       end
-      
+
+      it "should have the correct title" do
+        question.title.should eq FactoryGirl.attributes_for(:question)[:title]
+      end
+
+      it "should have the correct content" do
+        question.content.should eq FactoryGirl.attributes_for(:question)[:content]
+      end
+
+      it "should have the correct user_id" do
+        question.user_id.should eq 1
+      end
     end
 
     context "when passed no arguments" do
-      pending
+      it "should raise an error on create when passed 0 arguments" do
+        begin
+          FactoryGirl.create(:question, title: nil, content: nil, user_id: nil)
+        rescue Exception => e
+          e.should be_a ActiveRecord::StatementInvalid
+        end
+        # expect { question }.to raise_error(ArgumentError)
+        # p question.valid?
+        # expect(question ).to be_a ActiveRecord::StatementInvalid
+      end
+
     end
+
+    context "when no title provided" do
+      let(:question){FactoryGirl.build(:question, title: nil)}
+      # subject(:question)
+    end
+
+    it "raises an error when no title provided"
+
+    it "raises an error when no content provided"
+
+    it ""
+
 
   end
 
