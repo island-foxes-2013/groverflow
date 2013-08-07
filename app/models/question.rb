326749aeb78b5a8c24b29d2b4bfa8a82d13b73e4
Apprentice <apprentice@dbc19.local>
2013-08-07 16:45:21 -0700

    Modified question tests

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,6 @@
 class Question < ActiveRecord::Base
   attr_accessible :title, :content
+  validates :title, :content, presence: true
+  belongs_to :user
+  has_many :answers
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,61 +1,19 @@
 require 'spec_helper'
 
 describe Question do
-  # let(:question){FactoryGirl.new(:question)}
+  it { should be_a Question }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:content) }
+  it { should belong_to(:user) }
+  it { should have_many(:answers) }
 
-  describe "new" do
+    context "#new" do
+      subject { FactoryGirl.build(:question) }
 
-    context "when passed a title and content" do
-      let(:question){FactoryGirl.build(:question)}
-
-      it "should return an instance of question" do
-        expect(question).to be_a Question
-      end
-
-      it "should have the correct title" do
-        question.title.should eq FactoryGirl.attributes_for(:question)[:title]
-      end
-
-      it "should have the correct content" do
-        question.content.should eq FactoryGirl.attributes_for(:question)[:content]
-      end
-
-      it "should have the correct user_id" do
-        question.user_id.should eq 1
-      end
+      it { should be_a Question }
+      its(:title) { should eq FactoryGirl.attributes_for(:question)[:title] }
+      its(:content) { should eq FactoryGirl.attributes_for(:question)[:content] }
+      its(:user_id) { should eq FactoryGirl.attributes_for(:question)[:user_id] }
     end
-
-    context "when passed no arguments" do
-      it "should raise an error on create when passed 0 arguments" do
-        begin
-          FactoryGirl.create(:question, title: nil, content: nil, user_id: nil)
-        rescue Exception => e
-          e.should be_a ActiveRecord::StatementInvalid
-        end
-        # expect { question }.to raise_error(ArgumentError)
-        # p question.valid?
-        # expect(question ).to be_a ActiveRecord::StatementInvalid
-      end
-
-    end
-
-    context "when no title provided" do
-      let(:question){FactoryGirl.build(:question, title: nil)}
-      # subject(:question)
-    end
-
-    it "raises an error when no title provided"
-
-    it "raises an error when no content provided"
-
-    it ""
-
-
-  end
-
-  describe "save" do
-
-
-  end
 
 end
