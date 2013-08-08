1fb6297dab8a05443aed4dea600b4d20d57f5e3d
Apprentice <apprentice@dbc21.local>
2013-08-08 16:35:56 -0700

    added nested routes for answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,6 @@
 class Answer < ActiveRecord::Base
+  attr_accessible :content, :user, :best_answer
+
   validates :content, :best_answer, :user_id, :question_id , presence: true
   validates :question, associated: true
   belongs_to :user
