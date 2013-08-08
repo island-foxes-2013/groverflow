d78a8a1e982c552a04713fe5971d837a9cb019a5
Apprentice <apprentice@dbc21.local>
2013-08-08 15:46:00 -0700

    add tests to test that invalid and valid inputs on questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,12 +1,18 @@
 class QuestionsController < ApplicationController
-
  
   def new 
     @question = Question.new
   end
 
   def create
-    redirect_to root_path
+    @question = Question.create(params[:question])
+    unless @question.errors.empty?
+      @question.errors.full_messages.each {|msg| p msg}
+      @errors = @question.errors.full_messages
+      @question = Question.new(params[:question])
+      render 'new'
+    else
+      redirect_to question_path(@question)
+    end
   end
-
 end
