51b20b49f556af3e12c851207eebd23caa851afd
Apprentice <apprentice@dbc21.local>
2013-08-08 16:35:56 -0700

    added nested routes for answers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Groverflow::Application.routes.draw do
   resources :users, :only => [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
-  resources :questions, only: [:index, :new, :create, :show, :destroy]
+  resources :questions, only: [:index, :new, :create, :show, :destroy] do
+    resources :answers, only: [:index, :new, :create, :show, :destroy]
+  end
   match 'login' => 'sessions#new', :as => "login"
   root :to => 'questions#show'
 
