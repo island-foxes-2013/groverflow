8947c89d7f81fdca6ee6523011465dd3f06b749e
michaelchihuyho <michaelchihuyho@gmail.com>
2013-08-07 22:24:47 -0700

    Wrote some tests for logout feature

---
+++ spec/features/logout_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe 'log out' do
+  let(:user) { FactoryGirl.create(:user) }
+
+  context 'while on home page and logged in' do
+    before do
+      visit root_path
+      log_in
+    end  
+    subject{page}
+
+    it{should have_link("Logout")}
+
+    it "should redirect home after logging out" do
+      clink_link("Logout")
+      expect(current_path).to eq root_path
+    end
+  end
+end
