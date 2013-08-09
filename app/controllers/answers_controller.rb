17d09448437765689a0ecbc49b802052ff6a5ff0
michaelchihuyho <michaelchihuyho@gmail.com>
2013-08-09 16:51:48 -0700

    Implemented answer posting

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -4,6 +4,16 @@ class AnswersController < ApplicationController
   # end
 
   def create
+    p params
+    question = Question.find_by_id(params[:question_id])
+    @answer = current_user.answers.new(params[:answer])
+    question.answers << @answer
+    @answer.save
+    if @answer.errors.any?
+      flash[:answer_errors] = @answer.errors.full_messages
+      p @errors
+    end
+    redirect_to question_path(question)
   end
 
   # def destroy
