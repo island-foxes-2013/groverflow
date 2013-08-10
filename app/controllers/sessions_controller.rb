d6b40b161e4c523e5b91a8a1e0efb3323d1e3e4a
michaelchihuyho <michaelchihuyho@gmail.com>
2013-08-10 15:23:52 -0700

    Tidied up login error view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,14 +7,19 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by_email(params[:user][:email])
-    user = @user.authenticate(params[:user][:password])
-    if user
-      self.current_user = user
-      redirect_to root_path, notice: "You. Made it."
+    if @user
+      @user = @user.authenticate(params[:user][:password])
+      if @user
+        self.current_user = user
+        redirect_to root_path
+      else
+        flash.now[:login_errors] = "Invalid email/password combo"
+      end
     else
-      flash.now.alert = "Invalid username or password"
-      render :new
+      flash.now[:login_errors] = "Can't find that email!"
     end
+    @user = User.new(params[:user])
+    render :new
   end
 
   def destroy
