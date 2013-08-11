8bccc81bb69866116d1d794ce6d395088db2615a
jesseclay <jesseclay@gmail.com>
2013-08-11 16:10:05 -0700

    added basic seed file. NOTE: ADDED ATTR_ACCESSOR TO THE ANSWER CLASS FOR QUESTION to be able to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,17 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(
-  username: 'user',
-  email: "jesseclay@gmail.com",
+monster = User.create(
+  username: 'CIsForCookie',
+  email: "cookiemonster@gmail.com",
   password: "password")
+
+bird = User.create(
+  username: 'BigBird0723',
+  email: "bigfeathers@gmail.com",
+  password: "password")
+
+monster_q = monster.questions.create(title: "Are there any rehab facilities around Sesame Street?",
+  content: "Hey guys, this is a little embarassing but to try and stem my cookie addiction I decided to try black tar heroin. I'm now stuck in a downward spiral and can't find the light of day. Any suggestions?")
+
+Answer.create(user: bird, question: monster_q, content: "Happens to the best of us, lord knows I've had my fair share of court-mandated rehab visits. I'd recommend the 'Sundown Rehab Salon'")
