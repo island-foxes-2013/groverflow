e7615b4bdbc9d70c35f516bf259aa59463bdfdfb
michaelchihuyho <michaelchihuyho@gmail.com>
2013-08-10 19:11:16 -0700

    Fixed up bugs in sessions controller test code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,17 +7,17 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by_email(params[:user][:email])
     if @user
-      @user = @user.authenticate(params[:user][:password])
-      if @user
-        self.current_user = @user
+      @authenticated_user = @user.authenticate(params[:user][:password])
+      if @authenticated_user
+        self.current_user = @authenticated_user
         redirect_to root_path
       else
-        flash.now[:login_errors] = "Invalid email/password combo"
+        flash.now[:error] = "Invalid email/password combo"
         render :new
       end
     else
       @user = User.new(params[:user])
-      flash.now[:login_errors] = "Can't find that email!"
+      flash.now[:error] = "Can't find that email!"
       render :new
     end
     
