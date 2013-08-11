048930072677468b63ccd563062f1b8eefaa1048
Apprentice <apprentice@devbootcamp.com>
2013-08-11 16:08:51 -0700

    comment migration/model/tested +thought about sexy muppets to put on the site front page

---
+++ app/models/comment.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id               :integer          not null, primary key
+#  content          :text
+#  user_id          :integer
+#  commentable_id   :integer
+#  commentable_type :string(255)
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
+class Comment < ActiveRecord::Base
+  attr_accessible :user, :commentable, :content, :commentable_type
+
+  validates :user, :commentable_id, :commentable_type, :content, presence: true
+
+  belongs_to :user
+  belongs_to :commentable, polymorphic: true 
+end
