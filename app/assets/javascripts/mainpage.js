376a064658b381e776ab9b5666f97f738cbd3efb
michaelchihuyho <michaelchihuyho@gmail.com>
2013-08-12 01:32:20 -0700

    SO MUCH STYLING

--- app/assets/javascripts/mainpage.js
+++ app/assets/javascripts/mainpage.js
@@ -1,6 +1,6 @@
 $(function() {
-  var borderColors = ['Green', 'Orange', 'Yellow', 'Blue', 'Purple', 'Red'];
-  var backgroundColors = ['rgb(0, 204, 0)', 'rgb(255, 192, 77)', 'rgb(255, 255, 204)', 'rgb(153, 153, 255)', 'rgb(230, 0, 230)', 'rgb(255, 117, 117)'];
+  var borderColors = ['Green', 'Orange', 'Blue', 'Purple', 'Red'];
+  var backgroundColors = ['rgb(0, 204, 0)', 'rgb(255, 192, 77)', 'rgb(153, 153, 255)', 'rgb(230, 0, 230)', 'rgb(255, 117, 117)'];
   var borders = $('.question-summary');
 
   borders.each(function() {
@@ -9,9 +9,8 @@ $(function() {
     $(this).css("background-color", backgroundColors[colorIndex]);
   });
 
-  $('form').each(function() {
-    var colorIndex = Math.floor(Math.random() * borderColors.length);
-    $(this).css("border-color", borderColors[colorIndex]);
-    $(this).css("background-color", backgroundColors[colorIndex]);
-  });
+  // $('form').each(function() {
+  //   var colorIndex = Math.floor(Math.random() * borderColors.length);
+  //   $(this).css("background-color", backgroundColors[colorIndex]);
+  // });
 });
