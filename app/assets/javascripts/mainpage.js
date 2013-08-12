6e265366d8f51140fae0b71f36a1d379cad1d0db
jesseclay <jesseclay@gmail.com>
2013-08-11 22:15:34 -0700

    added background colors that match the borders for the front page

--- app/assets/javascripts/mainpage.js
+++ app/assets/javascripts/mainpage.js
@@ -1,8 +1,11 @@
 $(function() {
-  var colors = ['Green', 'Orange', 'Yellow', 'Blue', 'Purple', 'Red'];
+  var borderColors = ['Green', 'Orange', 'Yellow', 'Blue', 'Purple', 'Red'];
+  var backgroundColors = ['rgb(0, 204, 0)', 'rgb(255, 192, 77)', 'rgb(255, 255, 204)', 'rgb(153, 153, 255)', 'rgb(230, 0, 230)', 'rgb(255, 117, 117)'];
   var borders = $('.question-summary');
 
   borders.each(function() {
-    $(this).css("border-color", colors[Math.floor(Math.random() * colors.length)]);
+    var colorIndex = Math.floor(Math.random() * borderColors.length);
+    $(this).css("border-color", borderColors[colorIndex]);
+    $(this).css("background-color", backgroundColors[colorIndex]);
   });
 });
